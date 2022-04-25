99cff23621c6f4f354bab03e939cdfd0755606f1
Danny Kempkes <danny.kempkes@gmail.com>
2022-04-25 07:52:03 +0200

    fix: ok => nok

--- rundeck-reg.py
+++ rundeck-reg.py
@@ -1,6 +1,7 @@
 import requests
 from requests import get
 
+
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -11,6 +12,7 @@ class bcolors:
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
+
 
 name = "YOUR_NAME"
 username = "YOUR_USERNAME"
@@ -33,18 +35,21 @@ r = s.post(rundeck_url + "/api/27/job/e8507b3b-b82c-4454-af30-e128cd7dbb06/run",
            data=jobdata,
            headers=requestheaders)
 
-print(f"{bcolors.OKGREEN} Status Dev Tools: OK{bcolors.ENDC}") if r.status_code == 200 else print(f"{bcolors.FAIL} Status Dev Tools: OK{bcolors.ENDC}")
+print(f"{bcolors.OKGREEN} Status Dev Tools: OK{bcolors.ENDC}") if r.status_code == 200 else print(
+    f"{bcolors.FAIL} Status Dev Tools: NOK{bcolors.ENDC}")
 
 r = s.post(rundeck_url + "/api/27/job/9a190daf-3a6c-4d9f-8f28-e4d4a771ffbc/run",
            data=jobdata,
            headers=requestheaders)
 
-print(f"{bcolors.OKGREEN} Status Non-prod: OK{bcolors.ENDC}") if r.status_code == 200 else print(f"{bcolors.FAIL} Status Non-prod: NOK{bcolors.ENDC}")
+print(f"{bcolors.OKGREEN} Status Non-prod: OK{bcolors.ENDC}") if r.status_code == 200 else print(
+    f"{bcolors.FAIL} Status Non-prod: NOK{bcolors.ENDC}")
 
 r = s.post(rundeck_url + "/api/27/job/cde17503-06eb-4b09-b5fc-8388e0fa1902/run",
            data=jobdata,
            headers=requestheaders)
 
-print(f"{bcolors.OKGREEN} Status Prod: OK{bcolors.ENDC}") if r.status_code == 200 else print(f"{bcolors.FAIL} Status Prod: NOK{bcolors.ENDC}")
+print(f"{bcolors.OKGREEN} Status Prod: OK{bcolors.ENDC}") if r.status_code == 200 else print(
+    f"{bcolors.FAIL} Status Prod: NOK{bcolors.ENDC}")
 
 s.close()
